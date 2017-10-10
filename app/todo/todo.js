fabc6a0eb6d48ee3f008036a697b97847d294022
Mahendra <mahendra.singamsetty@srsconsultinginc.com>
2017-10-10 11:09:43 +0530

    controllers & filters

--- app/todo/todo.js
+++ app/todo/todo.js
@@ -8,10 +8,24 @@ var TodoItem = (function () {
 angular.module('todo', []);
 var TodoCtrl = (function () {
     function TodoCtrl() {
+        // this.todoItems.push(new TodoItem("Learning Typescript"));
+        // this.todoItems.push(new TodoItem("Learning AngularJS"));
         this.todoItems = [];
-        this.todoItems.push(new TodoItem("Learning Typescript"));
-        this.todoItems.push(new TodoItem("Learning AngularJS"));
+        this.newMsg = '';
     }
+    TodoCtrl.prototype.addNewItem = function () {
+        this.todoItems.push(new TodoItem(this.newMsg));
+        this.newMsg = '';
+    };
+    TodoCtrl.prototype.doneItems = function () {
+        var n = 0;
+        for (var i = 0; i < this.todoItems.length; i++) {
+            if (this.todoItems[i].status) {
+                n++;
+            }
+        }
+        return n;
+    };
     return TodoCtrl;
 }());
 angular.module('todo').controller("TodoCtrl", TodoCtrl);
--- app/todo/todo.ts
+++ app/todo/todo.ts
@@ -1,4 +1,9 @@
-class TodoItem {
+interface ITodoItem {
+    msg: string;
+    status: boolean;
+}
+
+class TodoItem implements ITodoItem {
     msg: string;
     status: boolean;
 
@@ -12,10 +17,28 @@ angular.module('todo', []);
 
 class TodoCtrl {
     todoItems: TodoItem[] = [];
+    newMsg: string;
 
     constructor() {
-        this.todoItems.push(new TodoItem("Learning Typescript"));
-        this.todoItems.push(new TodoItem("Learning AngularJS"));
+        // this.todoItems.push(new TodoItem("Learning Typescript"));
+        // this.todoItems.push(new TodoItem("Learning AngularJS"));
+
+        this.newMsg = '';
+    }
+
+    addNewItem() {
+        this.todoItems.push(new TodoItem(this.newMsg));
+        this.newMsg = '';
+    }
+
+    doneItems(): number {
+        let n = 0;
+        for(let i = 0; i < this.todoItems.length; i ++) {
+            if(this.todoItems[i].status) {
+                n++;
+            }
+        }
+        return n;
     }
 }
 
