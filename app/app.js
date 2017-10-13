83a507b12ecb927c78d87e047fe428e34791e646
Mahendra <mahendra.singamsetty@srsconsultinginc.com>
2017-10-13 10:22:44 +0530

    Rounting

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-var main = angular.module('angularWithTS', []);
+var main = angular.module('angularWithTS', ['ngRoute', 'products', 'todo']);
+main.config(routeConfig);
+routeConfig.$inject = ["$routeProvider"];
+function routeConfig($routeProvider) {
+    $routeProvider
+        .when("/products", {
+        templateUrl: "/app/products/products.html"
+    })
+        .when('/todo', {
+        templateUrl: "/app/todo/todo.html"
+    })
+        .when("/", {
+        template: "<h1>Welcome to My Angular Application</h1>"
+    })
+        .otherwise("/");
+}
